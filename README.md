89b8966c9bc4274de1e367261875ab8343a57574
Ignacio Anaya <ignacio.anaya89@gmail.com>
2015-07-14 23:56:15 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# node-explorer
+
+file-browser is a utility to browse files on your file system using your browser. Its equivalent of creating a file share that can be accessed over http. Using this you can share files between different machines, and across different operating systems. 
+
+### Install
+Open your terminal and type:
+```
+  npm install -g file-browser
+```
+
+### Usage
+Open your terminal and navigate to the desired root folder, then type:
+```
+  node-explorer
+```
+
+You can also set the port number that you want:
+```
+  node-explorer -p 3000
+```
+
+<p style="text-align: center">
+  <img src="img/sample.png" alt="node-explorer">
+</p>
+___
+See it on [NPM]().
---
+++ controller.js
@@ -0,0 +1,51 @@
+var colors = require('colors');
+var fs = require('fs');
+var path = require('path');
+var _ = require('lodash');
+
+function getFiles(req, res, dir) {
+  var currentDir =  dir;
+  var query = req.query.path || '';
+  var up = req.query.up;
+
+  if (query) {
+    currentDir = path.join(dir, query);
+  }
+
+  if (up) {
+    currentDir = query;
+  }
+
+  console.log('Browsing: '.green, currentDir.cyan);
+  
+  fs.readdir(currentDir, function (err, files) {
+    if (err) {
+      throw err;
+    }
+
+    var data = [];
+
+    files.filter(function (file) {
+      return true;
+    })
+    .forEach(function (file) {
+      try {
+        data.push({ 
+          name: file,
+          isDirectory: fs.statSync(path.join(currentDir, file)).isDirectory(), 
+          path: path.join(query, file),
+          ext: path.extname(file)
+        });
+      }
+      catch(e) {
+        console.log('Error: ' + e);
+      }
+
+    });
+    data = _.sortBy(data, function(f) { return f.Name; });
+    data.push(currentDir);
+    res.json(data);
+  });
+}
+
+module.exports.getFiles = getFiles;
